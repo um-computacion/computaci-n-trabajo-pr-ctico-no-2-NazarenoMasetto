aa1c3f3ae14b8586827858bd2f8b2371ad7362ee
Nazareno Masetto <nazamasetto@gmail.com>
2025-04-15 12:15:32 -0300

    Implementación de la función is_palindrome, limpieza de texto

--- src/palindrome.py
+++ src/palindrome.py
@@ -1,2 +1,8 @@
+import string
+
+def clean_text(text):
+    allowed = string.ascii_letters + string.digits
+    return ''.join(char.lower() for char in text if char.lower() in allowed)
+
 def is_palindrome(text):
     pass
