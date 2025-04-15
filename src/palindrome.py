3e4091060256ecd127aa7c9da761b22763d67c27
Nazareno Masetto <nazamasetto@gmail.com>
2025-04-15 13:06:46 -0300

    Implementación de la función is_palindrome, comparacion de caracteres

--- src/palindrome.py
+++ src/palindrome.py
@@ -7,3 +7,14 @@ def clean_text(text):
 def is_palindrome(text):
     cleaned = clean_text(text)
     return cleaned == cleaned[::-1]
+
+if __name__ == '__main__':
+    try:
+        while True:
+            entrada = input("Ingrese una palabra o frase: ")
+            if is_palindrome(entrada):
+                print(f'"{entrada}" es un palíndromo')
+            else:
+                print(f'"{entrada}" no es un palíndromo')
+    except KeyboardInterrupt:
+        print("\nPrograma finalizado.")
